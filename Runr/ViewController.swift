0e303657a0794c0ee92fb484c857caa802236880
Philip Sawyer <philsawyer94@gmail.com>
2018-08-25 19:19:53 -0400

    Initial Commit

---
+++ Runr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Runr
+//
+//  Created by Philip Sawyer on 8/25/18.
+//  Copyright © 2018 Philip Sawyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
