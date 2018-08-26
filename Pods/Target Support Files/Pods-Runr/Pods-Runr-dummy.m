2a8b088d75f2ab2ffdc0776bd8eae0cffa73d247
Philip Sawyer <philsawyer94@gmail.com>
2018-08-25 21:07:01 -0400

    Added realm pod

---
+++ Pods/Target Support Files/Pods-Runr/Pods-Runr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Runr : NSObject
+@end
+@implementation PodsDummy_Pods_Runr
+@end
---
+++ Pods/Target Support Files/Pods-RunrTests/Pods-RunrTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RunrTests : NSObject
+@end
+@implementation PodsDummy_Pods_RunrTests
+@end
